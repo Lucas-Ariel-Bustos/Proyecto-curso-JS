4c8281ea7ca9d223f14e5e40c01919713a424235
Proyecto de JavaScript <lucasbustos208@gmail.com>
2022-11-08 00:45:25 -0300

    Pre entrega 2 del curso de JS

--- js/index.js
+++ js/index.js
@@ -14,11 +14,40 @@ let precioPastas = 8000;
 let otraExcursion
 let precioTotal = 0;
 let invalido = false;
+//Arrays
+const bebidas = ['Coca-Cola', 'Agua con gas', 'Agua sin gas', 'Agua saborizada']
+
+//Objetos
+function Lugar (lugar, duracion, horarioSalida, horarioLlegada,distancia, paradas) {
+    this.lugar = lugar
+    this.duracion = duracion
+    this.horarioSalida = horarioSalida;
+    this.horarioLlegada = horarioLlegada;
+    this.distancia = distancia;
+    this.paradas =paradas;
+}
+const potrerillos = new Lugar ('Potrerillos','7hs','7hs', '14hs', '130km', '5');
+const potrerillosCompleto = new Lugar('Potrerillos Completo','8hs','7hs', '15hs', '164km', '8');
+const villavicencio = new Lugar ('Villavicencio','8hs','8:30hs', '16:30hs', '100km', '3');
+const uspallata = new Lugar ('Uspallata','8hs','8hs', '16hs', '240km', '5');
+const villavicencioUspallata = new Lugar ('Villavicencio-Uspallata','12hs','7hs', '19hs', '280km', '10')
+const puenteDelInca = new Lugar ('Puente del Inca', '9hs','7hs', '18hs', '400km', '10');
+const cristoRedentor = new Lugar ('Cristo Redentor','12hs','7hs', '19hs', '500km', '10');
+
+console.log(potrerillos);
+console.log(potrerillosCompleto);
+console.log(villavicencio);
+console.log(uspallata)
+console.log(villavicencioUspallata)
+console.log(puenteDelInca)
+console.log(cristoRedentor)
+
 
 function saludar(destino) {
-    alert("En el próximo menú elija su " + destino);
+    alert("Elija su " + destino);
 }
 saludar("destino");
+
 do {
     const excursionesMendoza = prompt(`¿Que lugar de Mendoza desea conocer?
     1. Potrerillos (dique)
@@ -76,9 +105,10 @@ do {
         break;
     }
     function saludar(destino) {
-        alert("En el próximo menú elija su " + destino);
+        alert("En la próxima lista elija su " + destino);
     }
     saludar("menú de comida");
+    
     const comidaIncluida = prompt(`Elija el menú que quiere consumir (todas con bebidas)
         1. Pollo con guarnición.
         2. Milanesas con guarnición.
@@ -108,13 +138,30 @@ do {
             invalido = true
             break;
     }   
-    if (comidaIncluida == 5) {
+    if (comidaIncluida === 5) {
         console.log("Sin Comida")
     }
-    // console.log("Menú adquirido " + comidaIncluida);
+    //Arrays
+    console.log('Estas son las opciones de bebidas', bebidas)
+   //find
+    let elijaBebida = prompt(`Escriba la bebida que haya elejido
+    Coca Cola
+    Agua con gas
+    Agua sin gas
+    Agua saborizad`)
+    console.log(bebidas.find((bebida) => bebida === elijaBebida));
+
     otraExcursion = prompt('¿Desea adquirir otra excursión? y/n');
-    console.log("Elija otra excursión ", otraExcursion);
-} while (otraExcursion == "y"); 
+    console.log('Elija otra excursión ', otraExcursion);    
+} while (otraExcursion === "y"); 
 console.log("Total a pagar " + precioTotal);
 
-
+//Correccíon del tutor!!
+while(otraExcursion != 'y'){
+    if (otraExcursion != 'n'){
+        alert('Ingrese una opcion valida');
+        otraExcursion = prompt('¿Desea adquirir otra excursión? y/n');
+    }else{
+        break;
+    }
+}
